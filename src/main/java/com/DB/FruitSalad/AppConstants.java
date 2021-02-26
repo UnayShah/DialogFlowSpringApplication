e308c611b54608e0b2e45e2a8f8f4b3158c2e7ec
bz_2505 <bhagyashreezala1@gmail.com>
2021-02-26 12:18:05 +0530

    extracted value

--- src/main/java/com/DB/FruitSalad/AppConstants.java
+++ src/main/java/com/DB/FruitSalad/AppConstants.java
@@ -11,5 +11,6 @@ public interface AppConstants {
 	final public static String FORM_AGENT = "3ebfe9c9-10de-4369-bb95-16e1005e8f9a";
 	final public static String MANUAL_OR_AUDIO_AGENT = "3d1f2c36-feea-4c2e-9fd8-4f217d0b2565"; // need to set
 	final public static String MATCH_TYPE = "match_type";
+    final public static String EXTRACTED_VALUE = "extracted_value";
 	final public static String LANGUAGE_CODE = "en-US";
 }
--- src/main/java/com/DB/FruitSalad/Controller/IntentAPI.java
+++ src/main/java/com/DB/FruitSalad/Controller/IntentAPI.java
@@ -15,8 +15,11 @@ import com.google.api.gax.rpc.ApiException;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @RestController
 public class IntentAPI {
@@ -115,12 +118,26 @@ public class IntentAPI {
         Map<String, String> responseMap = new HashMap<>();
         for (Map.Entry<String, QueryResult> entry : map.entrySet()) {
             QueryResult res = entry.getValue();
-            /**
-             *
-             * Remaining
-             * add match - > field -> string_value
-             *
-             */
+            String str = res.getMatch().getParameters().getFieldsMap().toString();
+            str = str.replaceAll("\\s","");
+            String extracted = null;
+            int index1=str.indexOf("original");
+            if(index1!=-1){
+                str = str.substring(index1,str.length()-1);
+                index1 = str.indexOf("string_value:");
+                str=str.substring(index1,str.length()-1);
+                str = str.replaceAll("\"","").replaceAll("string_value:","");
+                index1=str.indexOf("}");
+                extracted= str.substring(0,index1);
+            } else {
+                index1 = str.indexOf("string_value:");
+                if(index1!=-1){
+                    str=str.substring(index1,str.length()-1);
+                    extracted = str.replaceAll("\"","").replaceAll("string_value:","").replaceAll("}","");
+                }
+            }
+
+            responseMap.put(AppConstants.EXTRACTED_VALUE,extracted);
             responseMap.put(AppConstants.PAGE_TYPE, AppConstants.FORM_KEY);
             responseMap.put(AppConstants.MATCH_TYPE, res.getMatch().getMatchType().toString());
             responseMap.put(AppConstants.RESPONSE_TEXT_KEY, res.getResponseMessages(res.getResponseMessagesCount() - 1).getText().getText(0));
